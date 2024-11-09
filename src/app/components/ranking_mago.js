6df0b0c25f6c8cb6b84062892bda619226153b34
lucasmacedobernardino <lucasmacedoes@gmail.com>
2024-11-08 21:22:40 -0300

    cabo

--- src/app/components/ranking_mago.js
+++ src/app/components/ranking_mago.js
@@ -6,7 +6,7 @@ import Link from "next/link"
 export default function RankingMago({ src, alt, title, href, translate }) {
     return (
         <Link href={href ? href : ''} className={href ? "cursor-pointer" : "cursor-default"}>
-            <div className={'flex flex-col items-center justify-center' + ` ${translate}`}>
+            <div className={'text-black flex flex-col items-center justify-center' + ` ${translate}`}>
                 <Image src={src} width={62} height={62} alt={alt} priority />
                 <p className='text-[10px] font-bold'>
                     {title}
